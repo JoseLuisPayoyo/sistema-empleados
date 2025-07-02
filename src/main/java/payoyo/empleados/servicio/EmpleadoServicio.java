37cdc74e5c25c5afa9df29f71947bedc201c36b6
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-07-02 13:09:15 +0200

    Implementación de EmpleadoServicio con operaciones CRUD

--- src/main/java/payoyo/empleados/servicio/EmpleadoServicio.java
+++ src/main/java/payoyo/empleados/servicio/EmpleadoServicio.java
@@ -1,4 +1,36 @@
 package payoyo.empleados.servicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import payoyo.empleados.modelo.Empleado;
+import payoyo.empleados.repositorio.EmpleadoRepositorio;
+
+import java.util.List;
+
+@Service
 public class EmpleadoServicio implements IEmpleadoServicio{
+
+    @Autowired
+    private EmpleadoRepositorio empleadoRepositorio;
+
+    @Override
+    public List<Empleado> listarEmpleados() {
+        return empleadoRepositorio.findAll();
+    }
+
+    @Override
+    public Empleado buscarEmpleadoPorId(Integer idEmpleado) {
+        Empleado empleado = empleadoRepositorio.findById(idEmpleado).orElse(null);
+        return empleado;
+    }
+
+    @Override
+    public void guardarEmpleado(Empleado empleado) {
+        empleadoRepositorio.save(empleado);
+    }
+
+    @Override
+    public void eliminarEmpleado(Empleado empleado) {
+        empleadoRepositorio.delete(empleado);
+    }
 }
